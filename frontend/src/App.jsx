cc0b93ac32d4ad7331b5ba3cb37cbb32c2860957
sincerelyyyash <yta0088@gmail.com>
2024-01-27 22:01:22 +0530

    React routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 
+import { BrowserRouter, Route, Routes }from "react-router-dom";
+import Signin from "./Components/Signin";
+import Signup from "./Components/Signup";
+import Send from "./Components/Send";
+import Dashboard from "./Components/Dashboard";
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/send" element={<Send/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/Components/Signup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className='flex flex-col px-10 py-10'>
+        <div className='flex justify-center text-black text-4xl font-bold'>
+            Sign Up
+        </div>
+        <div className='flex justify-center mt-4 text-gray-500'>
+            Enter your information to create an account
+        </div>
+
+    </div>
+  )
+}
+
+export default Signup
