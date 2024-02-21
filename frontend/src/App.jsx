a9933c43db56f99f2f040616146870c7b66bfd29
sincerelyyyash <yta0088@gmail.com>
2024-02-21 23:50:28 +0530

    Route controller

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Signin/>}/>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/send" element={<Send/>}/>
